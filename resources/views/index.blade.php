8da8230d75e38a0f457bda9e3348873e335dc154
M. Hanif <hanif@example.com>
2026-05-25 12:04:07 +0700

    Pertemuan 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,28 @@
-<!DOCTYPE html>
-<html>
+@extends('template')
+@section('title', 'Data Pegawai')
+@section('konten')
 
-<head>
-    <title>Tutorial Membuat Pencarian Pada Laravel - www.malasngoding.com</title>
-</head>
-
-<body>
-
-
-
-    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-    <h3>Data Pegawai</h3>
-
-
-    <p>Cari Data Pegawai :</p>
-    <form action="/pegawai/cari" method="GET">
-        <input type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old('cari') }}">
-        <input type="submit" value="CARI">
-    </form>
-
+    <a href="/pegawai/tambah" class="btn btn-primary"> Tambah Pegawai Baru</a>
+    <br />
     <br />
 
-    <table border="1">
+    <form action="/pegawai/cari" method="GET" class="form-inline">
+        <div class="from-group">
+            <label>Cari Data Pegawai :</label><input type="text" name="cari" placeholder="Cari Pegawai .."
+                class="form-control">
+
+            <input type="submit" value="CARI" class="btn btn-light">
+        </div>
+    </form>
+    <br />
+
+    <table class="table table-striped table-hover">
         <tr>
             <th>Nama</th>
             <th>Jabatan</th>
             <th>Umur</th>
             <th>Alamat</th>
+            <th>Opsi</th>
         </tr>
         @foreach ($pegawai as $p)
             <tr>
@@ -34,13 +30,13 @@
                 <td>{{ $p->pegawai_jabatan }}</td>
                 <td>{{ $p->pegawai_umur }}</td>
                 <td>{{ $p->pegawai_alamat }}</td>
+                <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+                    &ensp;
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+                </td>
             </tr>
         @endforeach
     </table>
-
-
-
-
-</body>
-
-</html>
+    {{ $pegawai->links() }}
+@endsection
